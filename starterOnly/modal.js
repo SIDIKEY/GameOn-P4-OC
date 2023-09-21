a7d8a8edd53945849f024f95efcd07fcdf26e6fd
SIDIKEY <sidikikeitadixinne@gmail.com>
2023-09-21 10:00:43 +0200

    input implementation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,16 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnCloseModal = document.querySelector(".close");
+const form = document.querySelector('form')
+
+// form
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locations = document.querySelectorAll("input[name='location']")
+const conditions = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
